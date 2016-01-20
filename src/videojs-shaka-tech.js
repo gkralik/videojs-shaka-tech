7ef648794a86acb1954d7ccc5b9fc87f8657e994
Gregor Kralik <g.kralik@gmail.com>
2016-01-20 10:42:32 +0100

    initial commit

---
+++ src/videojs-shaka-tech.js
@@ -0,0 +1,9 @@
+/**
+ * videojs-shaka-tech.js
+ *
+ * Use of this source code is governed by a license that can be
+ * found in the LICENSE file.
+ *
+ * @copyright 2016 Gregor Kralik
+ * @author Gregor Kralik <g.kralik@gmail.com>
+ */
